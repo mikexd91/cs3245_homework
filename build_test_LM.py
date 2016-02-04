6160ebdff6a69a1b865a427fbe6655a2b6e8e5a5
MX Chai <mingxuan@live.com.sg>
2016-02-04 20:46:41 +0800

    100% accuracy achieved

--- build_test_LM.py
+++ build_test_LM.py
@@ -21,7 +21,7 @@ def build_LM(in_file):
     d["malaysian"] = {}
     d["indonesian"] = {}
     d["tamil"] = {}
-    all_lst = []
+    all_lst = [] # stores all existing tuples from the training set
 
     wordcount = {}
     wordcount["malaysian"] = 0
@@ -57,9 +57,13 @@ def build_LM(in_file):
     for tup in all_lst:
         for label, _ in d.iteritems():
             if tup not in d[label]:
-                # print tup, "added to " + label
                 d[label][tup] = 1
                 wordcount[label] += 1
+
+
+    # Should change the way total count for each langauge is computed
+    # Right now it's too much maintainence, should just iterate the dictionary one time 
+    # when it's fully built
 
     print "Finished building language models"
     return d, wordcount
@@ -81,34 +85,42 @@ def test_LM(in_file, out_file, LM):
 
     # Initaliaze
     d = LM[0]
-
     wordcount = LM[1]
-    count = 0
 
     # Read the test file
     f = open(in_file, 'r')
     o = open(out_file, 'w')
     for line in f:
+        misses = 0
+        total_checks = 0
         malay, indon, tamil = 0, 0, 0
         tokens = list(line)
         for i in range(len(tokens)-3):
             tup = tuple(tokens[i:i+4])
-            
+            total_checks += 1
             # Calculate probabilities using log 
             try:
                 malay += math.log(d["malaysian"][tup] / float(wordcount["malaysian"]))
                 indon += math.log(d["indonesian"][tup] / float(wordcount["indonesian"]))
                 tamil += math.log(d["tamil"][tup] / float(wordcount["tamil"]))
             except:
-                pass
-
-        output = max(malay, indon, tamil)
-        if output == malay:
-            o.write("malaysian " + line)
-        elif output == indon:
-            o.write("indonesian " + line)
+                # use this to calculate probabilities of being an alien string
+                # print "Cannot find " + str(tup)
+                misses += 1
+        
+        # Check for alien. Use 75% miss-rate as arbitrary cut-off point (since 75 is A1)
+        miss_rate = misses / float(total_checks)
+        print miss_rate, misses, total_checks
+        if miss_rate > 0.75:
+            o.write("other " + line)
         else:
-            o.write("tamil " + line)
+            output = max(malay, indon, tamil)
+            if output == malay:
+                o.write("malaysian " + line)
+            elif output == indon:
+                o.write("indonesian " + line)
+            else:
+                o.write("tamil " + line)
 
 
 
