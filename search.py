9f75ac51b158200dee34ce3efe2bf70695b4b0e5
MX Chai <mingxuan@live.com.sg>
2016-03-16 23:03:01 +0800

    Cleaned up search.py

--- search.py
+++ search.py
@@ -17,6 +17,9 @@ def build_dict(input_dict_file):
     dictionary = {}
     document_length = 0
 
+    # dictionary format:
+    # { term: (byte_offset, document_frequency), ... }
+
     for line in dict_file:
         split_line = line.strip().split(" ")
         
@@ -45,10 +48,6 @@ def execute_queries(input_post_file, input_query_file, output_file, dictionary, 
     postings = file(input_post_file, 'r')
     output = file(output_file, 'w')
     stemmer = nltk.stem.porter.PorterStemmer()
-    doc_score_lst = [] # used to store all the (document, score) tuples
-
-    query_lst = []
-    document_lst = []
 
     # Dictionary to store all the scores
     table = {}
@@ -77,10 +76,9 @@ def execute_queries(input_post_file, input_query_file, output_file, dictionary, 
                 idf = math.log((float(document_length)/document_freq), 10)
                 table["idf"][term] = idf # store the idf of this term into the dictionary
 
-            # Input the NORMALIZED tf (read from the postings file) for the term in every document
+            # Read the NORMALIZED wt (read from the postings file) of the term in every document
             byte_offset = dictionary[term][0]
-            # print (term, dictionary[term][0])
-            posting_reader = NoobReader(postings, byte_offset)  
+            posting_reader = PostingReader(postings, byte_offset)  
             containing_docs = posting_reader.to_list()
 
             # Build the rows
@@ -88,55 +86,27 @@ def execute_queries(input_post_file, input_query_file, output_file, dictionary, 
                 if doc_id not in table:
                     table[doc_id] = {}
                 table[doc_id][term] = freq     
-
-            # Calculate the score for term
-            # Query scores don't change, they remain constant throughout the query
-            # ltc scheme: NORMALIZE((1+lg(tf)) * idf)
-
-            # Calculate the score for document
-            # Document scores change per document
-
-            # Pseudo code
-            # 1. Read in all the documents that contain the query terms
-            # 2. Calculate lnc score for the document - maintain a mapping from document id to document score
-            # 3. After all scores have been calculated, sort them and return top 10
             
-        # Normalising can only be done after every term has been looked at  
-        # do the normalising here
-        # print "outside the loop: ", table        
+        # Normalising for documents can only be done after every term has been looked at
+        # Same goes for the calculation of score
+        # Do the normalising here
 
-        # Overwrite the values with the tf-idf score for query (the values used to be raw tf)
+        # Overwrite the values with the tf-idf weight for query (the values used to be raw tf)
         for (term, freq) in table["query"].items():
             table["query"][term] = (1+math.log(table["query"][term], 10)) * table["idf"][term]
 
-        # Construct normalised value for query column
+        # Construct normalised weight for query column
         unit_length = reduce(lambda x, y: x+y, map(lambda x: x[1]**2, table["query"].items()))
         unit_length = math.sqrt(unit_length)
         for (term, freq) in table["query"].items():
             # Overwrite the values with the normalised weight
             table["query"][term] = table["query"][term] / unit_length
-            # print term, table["idf"][term]
-
-        # Construct normalised value for every document
+        
+        # Construct (doc_id, all query terms) score
         all_doc_ids = table.keys()
         all_doc_ids.remove("idf")
         all_doc_ids.remove("query")
 
-        # # Overwrite the values with the normalised score (the values used to be raw tf)
-        # for doc_id in all_doc_ids:
-        #     doc_dct = table[doc_id]
-        #     print doc_dct
-        #     doc_length = 0
-            
-        #     doc_length = reduce(lambda x, y: x+y, map(lambda x: x[1]**2, doc_dct.items()))
-        #     doc_length = math.sqrt(doc_length)
-
-        #     for (term, freq) in doc_dct.items():
-        #         doc_dct[term] = doc_dct[term] / doc_length
-
-        # print table # by this step, all the documents have a correct normalised weight for each term
-
-        # Construct (doc_id, all query terms) score
         doc_score = {}
         for doc_id in all_doc_ids:
             doc_score[doc_id] = 0
@@ -144,17 +114,16 @@ def execute_queries(input_post_file, input_query_file, output_file, dictionary, 
         for (term, wt) in table["query"].items():
             for doc_id in all_doc_ids:
                 if term in table[doc_id]: # if the ith term of the query is present in the document
-                    doc_score[doc_id] += table[doc_id][term] * wt
+                    doc_score[doc_id] += table[doc_id][term] * wt # table[doc_id][term] contains the normalised wt for term
 
-        # print doc_score
         # print sorted(doc_score.items(), key=lambda x: x[1], reverse=True)[:10]
-        foo = map(lambda x: x[0], filter(lambda x: x[1] > 0, sorted(doc_score.items(), key=lambda x: (x[1], x[0]), reverse=True)[:10]))
+        top_10 = map(lambda x: x[0], filter(lambda x: x[1] > 0, sorted(doc_score.items(), key=lambda x: (x[1], x[0]), reverse=True)[:10]))
     
         # Write to file
-        output_line = reduce(lambda x, y: x + str(y) + " ", foo, "").strip() + "\n"
+        output_line = reduce(lambda x, y: x + str(y) + " ", top_10, "").strip() + "\n"
         output.write(output_line)
 
-class NoobReader:
+class PostingReader:
     def __init__(self, postings_file, byte_offset):
         self.postings_file = postings_file
         self.byte_offset = byte_offset
@@ -213,5 +182,4 @@ if input_dict_file == None or input_post_file == None or input_query_file == Non
 
 # Execution
 (dictionary, document_length) = build_dict(input_dict_file)
-# print dictionary
 execute_queries(input_post_file, input_query_file, output_file, dictionary, document_length)
